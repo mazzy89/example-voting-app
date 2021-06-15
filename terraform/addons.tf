3229d2bd3ecdaca266db43a4882de5e539f7d95d
Salvatore Mazzarino <dev@mazzarino.cz>
2021-06-15 16:39:07 +0200

    Add Prometheus and Redis addons in Iac
    Signed-off-by: Salvatore Mazzarino <dev@mazzarino.cz>

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -6,3 +6,29 @@ resource "helm_release" "traefik" {
 
   namespace = "kube-system"
 }
+
+resource "helm_release" "kube_prometheus_stack" {
+  name = "prometheus"
+
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus-community/kube-prometheus-stack"
+
+  create_namespace = true
+  namespace        = "monitoring"
+
+  set {
+    name  = "alertmanager.enabled"
+    value = "false"
+  }
+}
+
+
+resource "helm_release" "redis" {
+  name = "redis"
+
+  repository = "https://charts.bitnami.com/bitnami"
+  chart      = "bitnami/redis"
+
+  create_namespace = true
+  namespace        = var.do_project_name
+}
