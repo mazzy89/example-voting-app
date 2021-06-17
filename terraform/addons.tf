a8f0abbfbfef56239a940a9b61fe310cf1427cb6
Salvatore Mazzarino <dev@mazzarino.cz>
2021-06-18 00:22:54 +0200

    Fix nginx
    Signed-off-by: Salvatore Mazzarino <dev@mazzarino.cz>

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -2,7 +2,7 @@ resource "helm_release" "ingress_nginx" {
   name = "ingress-nginx"
 
   repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "nginx"
+  chart      = "ingress-nginx"
 
   namespace = "kube-system"
 
@@ -10,6 +10,13 @@ resource "helm_release" "ingress_nginx" {
     name  = "controller.publishService.enabled"
     value = "true"
   }
+
+
+  set {
+    name  = "controller.addHeaders.X-Forwarded-For"
+    value = "https"
+  }
+
 
   set {
     name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/do-loadbalancer-protocol"
