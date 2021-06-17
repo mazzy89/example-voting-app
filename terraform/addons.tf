daf11ac9a1e5a427a2941e558741b460bc2093c3
Salvatore Mazzarino <dev@mazzarino.cz>
2021-06-17 23:31:25 +0200

    Fix dep 2
    Signed-off-by: Salvatore Mazzarino <dev@mazzarino.cz>

--- terraform/addons.tf
+++ terraform/addons.tf
@@ -196,7 +196,7 @@ resource "helm_release" "app" {
   }
 
   depends_on = [
-    helm.ingress_nginx
+    helm_release.ingress_nginx
   ]
 }
 
@@ -235,6 +235,6 @@ resource "helm_release" "external_dns" {
   }
 
   depends_on = [
-    helm.ingress_nginx
+    helm_release.ingress_nginx
   ]
 }
